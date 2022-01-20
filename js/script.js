9ab1857b73e6b0ebfc32f3466c811a89cd58112e
nixon2105 <radivilov1991@mail.ru>
2022-01-20 14:25:59 +0300

    feat: add event change background and sound

--- js/script.js
+++ js/script.js
@@ -1,44 +1,52 @@
+const listBtn = document.querySelector('.main-nav__list');
 const btn = document.querySelector('.main-nav__btn');
 const playBtn = document.querySelector('.play');
-// const pauseBtn = document.querySelector('.pause');
-const listBtn = document.querySelector('.main-nav__list');
+const item = document.querySelector('.main-nav__item');
+const main = document.querySelector('.main');
 
 const sounds = ['colibri', 'drozd', 'icterey', 'sinica', 'snegir'];
 
 const audio = new Audio();
+
 const playSounds = (e) => {
-  btn.classList.add('active');
-  playBtn.classList.remove('play');
-  playBtn.classList.add('pause');
-  if (e.target.getAttribute('data-sound') === 'sinica') {
-    audio.src = '../assets/audio/sinica.mp3';
-    audio.play();
-  }
-  // if (e.target.getAttribute('data-sound') === 'drozd') {
-  //   audio.src = '../assets/audio/drozd.mp3';
-  //   audio.play();
-  // }
+  playBtn.classList.toggle('pause');
+  audio.play();
 };
 
 const pauseSounds = (e) => {
-  if (e.target.classList.contains('pause')) {
-    playBtn.classList.remove('pause');
-    playBtn.classList.add('play');
-    audio.pause();
-  } else if (e.target.classList.contains('play')) {
-    playBtn.classList.remove('play');
-    playBtn.classList.add('pause');
-    audio.play();
-  }
-
-  // btn.forEach((el) => console.log(el.getAttribute('data-sound')));
-  // if (e.target.getAttribute('data-sound') === 'sinica') {
-  //   audio.src = '../assets/audio/sinica.mp3';
-  //   audio.play();
-  // }
+  // playBtn.classList.add('pause');
+  // playBtn.classList.add('play');
+  audio.pause();
 };
 
-// a();
+const loadImage = () => {};
 
-btn.addEventListener('click', playSounds);
+listBtn.addEventListener('click', (e) => {
+  if (e.target.closest('.main-nav__btn')) {
+    let action = e.target.getAttribute('data-item');
+    if (action === 'solovey') {
+      audio.src = '../assets/audio/solovey.mp3';
+      main.style.backgroundImage = 'url(../assets/images/solovey.jpg)';
+    }
+    if (action === 'drozd') {
+      audio.src = '../assets/audio/drozd.mp3';
+      main.style.backgroundImage = 'url(../assets/images/drozd.jpg)';
+    }
+    if (action === 'malinovka') {
+      audio.src = '../assets/audio/zarynka.mp3';
+      main.style.backgroundImage = 'url(../assets/images/zarynka.jpg)';
+    }
+    if (action === 'javoronok') {
+      audio.src = '../assets/audio/javoronok.mp3';
+      main.style.backgroundImage = 'url(../assets/images/javoronok.jpg)';
+    }
+    if (action === 'slavka') {
+      audio.src = '../assets/audio/slavka.mp3';
+      main.style.backgroundImage = 'url(../assets/images/slavka.jpg)';
+    }
+    playSounds();
+    // loadImage();
+  }
+});
+
 playBtn.addEventListener('click', pauseSounds);
