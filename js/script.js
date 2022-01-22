5ea3f2fd02e234e954f15136c743bc0f100cc593
nixon2105 <radivilov1991@mail.ru>
2022-01-22 10:25:25 +0300

    feat: add change imges logo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const logo = document.querySelector('.header-logo');
 const listBtn = document.querySelector('.main-nav__list');
 const btn = document.querySelector('.main-nav__btn');
 const playBtn = document.querySelector('.play');
@@ -29,22 +30,27 @@ listBtn.addEventListener('click', (e) => {
     if (action === 'solovey') {
       audio.src = '../assets/audio/solovey.mp3';
       main.style.backgroundImage = 'url(../assets/images/solovey.jpg)';
+      logo.style.backgroundImage = 'url(../assets/images/icon-solovey.png)';
     }
     if (action === 'drozd') {
       audio.src = '../assets/audio/drozd.mp3';
       main.style.backgroundImage = 'url(../assets/images/drozd.jpg)';
+      logo.style.backgroundImage = 'url(../assets/images/icon-drozd.png)';
     }
     if (action === 'malinovka') {
       audio.src = '../assets/audio/zarynka.mp3';
       main.style.backgroundImage = 'url(../assets/images/zarynka.jpg)';
+      logo.style.backgroundImage = 'url(../assets/images/icon-malinovka.png)';
     }
     if (action === 'javoronok') {
       audio.src = '../assets/audio/javoronok.mp3';
       main.style.backgroundImage = 'url(../assets/images/javoronok.jpg)';
+      logo.style.backgroundImage = 'url(../assets/images/iconn-zarynka.png)';
     }
     if (action === 'slavka') {
       audio.src = '../assets/audio/slavka.mp3';
       main.style.backgroundImage = 'url(../assets/images/slavka.jpg)';
+      logo.style.backgroundImage = 'url(../assets/images/icon-slavka.png)';
     }
     toggleBtnSounds();
   }
